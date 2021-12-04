83f10b5524f656b2a94c0ecc0870ac31873d48ae
harshitadatra <95231941+Gautam2305@users.noreply.github.com>
2021-12-04 20:21:08 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var btnTranslate = document.querySelector("#btn-translate");
-btnTranslate.addEventListener("click",function
-    clickEventHandler(){
-        console.log("clicked!")
-    }
-)
-var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
-function clickHandler(){
+var outputDiv = document.querySelector("#output");
+
+
+function clickHandler() {
     console.log("clicked!");
     console.log("input", txtInput.value);
 };
+
+var txtInput = document.querySelector("#txt-input");
+console.log(txtInput);
+
+
+btnTranslate.addEventListener("click",clickHandler)
+
+ 
+// outputDiv.innerText="Gautam Adatra";
+function clickHandler(){
+    outputDiv.innerText = "The meaning is: "+ txtInput.value;
+
+}
